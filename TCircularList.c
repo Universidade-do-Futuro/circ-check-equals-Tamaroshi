9826b804b3ddfd436f00f85cec7b1ada63b81ede
Luis Felipe de Oliveira Tamashiro <luis.tamashiro@mail.uft.edu.br>
2022-05-24 18:43:18 -0300

    Arquivos atualizados.

--- TCircularList.c
+++ TCircularList.c
@@ -1,90 +1,162 @@
 #include "TCircularList.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#define END 0
 #define BEGIN 1
+#define END 0
 
-typedef struct no {
+typedef struct no
+{
     int info;
-    struct no* prox;
+    struct no *prox;
 }TNo;
 
-TNo* TNo_createNFill(int);
-_Bool TCircularList_insert(TCircularList*, int, char);
-
-struct linked_list{
+struct circular_list
+{
     TNo* inicio;
     int qty;
 };
 
-TCircularList* TCircularList_create(){
-    TCircularList* nova = malloc(sizeof(TCircularList));
-    if(nova!=NULL){
-        nova->inicio = NULL;
-        nova->qty = 0;
-    } return nova;
+TNo* TNo_Fill(int);
+int TCircularList_CountNo(TCircularList*);
+
+TCircularList *TCircularList_Create()
+{
+    TCircularList *novo = malloc(sizeof(TCircularList));
+    if(novo != NULL)
+        novo->inicio = NULL;
+    
+    return novo;
 }
 
-_Bool TCircularList_insert_end(TCircularList* lista, int info){
-    return TCircularList_insert(lista, info, END);
-}
-
-void TCircularList_print(TCircularList* lista){
+void TCircularList_Print(TCircularList* lista){
     TNo* aux = lista->inicio;
 
     do{
         if(aux==NULL) break;
-        printf("[%d]-> ", aux->info);
+        printf("[%d]->", aux->info);
         aux = aux->prox;
     }while(aux != lista->inicio);
     putchar('\n');
 }
 
-//Auxiliares
-
-TNo* TNo_createNFill(int info){
-    TNo* novo = malloc(sizeof(TNo));
-    if(novo!=NULL){
-        novo->info = info;
-        novo->prox = NULL;
-    } return novo;
+_Bool TCircularList_Insert_Begin(TCircularList* lista, int info)
+{
+    TCircularList_Insert(lista, info, BEGIN);
 }
 
-_Bool TCircularList_insert(TCircularList* lista, int info, char flag){
-    TNo* novo = TNo_createNFill(info);
-    if(novo==NULL) return false;
-    
-    if(lista->inicio == NULL){
+_Bool TCircularList_Insert_End(TCircularList* lista, int info)
+{
+    TCircularList_Insert(lista, info, END);
+}
+
+_Bool TCircularList_Insert(TCircularList* lista, int info, char flag)
+{
+    TNo *novo = TNo_Fill(info);
+    if(novo == NULL)
+        return false;
+
+    if(lista->inicio == NULL)
+    {
         novo->prox = novo;
         lista->inicio = novo;
     }
-    else{
-        //Procurar o elemento do fim
+    else
+    {
         TNo* aux = lista->inicio;
-        while(aux->prox != lista->inicio)
+        while (aux->prox !=lista->inicio)
             aux = aux->prox;
         aux->prox = novo;
         novo->prox = lista->inicio;
     }
+
     if(flag == BEGIN)
         lista->inicio = novo;
-    lista->qty++;
     return true;
 }
 
+_Bool TCircularList_equals(TCircularList* lista1, TCircularList* lista2)
+{
+    if(lista1->inicio == NULL && lista2->inicio == NULL) return false;
 
-bool TCircularList_equals(TCircularList* lista1, TCircularList* lista2){
-    /**
-    Campos existentes na TCircularList {
-        qty -> quantidade de elementos na lista
-        inicio-> ponteiro para o início da lista
+    int a, b;
+    a = TCircularList_CountNo(lista1);
+    b = TCircularList_CountNo(lista2);
+
+    if(a != b)
+        return false;  
+
+    else
+    {
+        TNo* aux = lista1->inicio;
+        TNo* aux2 = lista2->inicio;
+        int flag = 1, count=0, c=0;
+        if(aux->info != aux2->info)
+        {
+            while (aux->info != aux2->info)
+            {
+                aux2 = aux2->prox;
+                if(aux2 == lista2->inicio)
+                {
+                    flag = 0;
+                    return false;
+                    break;
+                }
+            }
+
+            if(flag = 1)
+            {
+                while (c < a)
+                {
+                    if(aux->info == aux2->info)
+                    {
+                        count++;
+                        aux = aux->prox;
+                        aux2 = aux2->prox;
+                    }
+                    c++;
+                }
+            }
+
+            else return false;
+            if(count == a) return true;
+        }
     }
-    Campos da TNo{
-        int info;
-        struct no* prox;
+}
+
+void TCircularList_Scanear(TCircularList* lista)
+{
+    int size, info;
+    scanf("%d", &size);
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &info);
+        TCircularList_Insert_End(lista, info);
     }
-    */
-    //Seu código aqui.... 
-    return false;
+}   
+
+//Auxiliares
+
+TNo* TNo_Fill(int info)
+{
+    TNo* novo = malloc(sizeof(TNo));
+    if(novo != NULL)
+    {
+        novo->info = info;
+        novo->prox = NULL;
+    }
+
+    return novo;
+}
+
+
+int TCircularList_CountNo(TCircularList* lista)
+{
+    int count=0;
+    TNo *aux = lista->inicio;
+    do
+    {
+        count += 1;
+        aux = aux->prox;
+    } while (aux != lista->inicio);
+    return count;
 }
--- TCircularList.h
+++ TCircularList.h
@@ -1,31 +1,17 @@
-#ifndef TCIRCULAR_LINKED_LIST_H
-#define TCIRCLAR_LINKED_LIST_H
+#ifndef TCIRCULARLIST_H
+#define TCIRCULARLIST_H
 #include <stdbool.h>
-   
-typedef struct linked_list TCircularList;
-/**
- * Cria uma uma instância válida de TCircularList
- * @return TCircularList*
- * */
-TCircularList* TCircularList_create();
-/**
- * Insere um inteiro no fim da lista.
- * @param TCircularList* ponteiro da lista
- * @param int informação a ser inserida
- * @return _Bool (se deu certo ou não)
- * */
-_Bool TCircularList_insert_end(TCircularList*, int);
-/**
- * Remove o nó após n passos na lista circular a partir do início.
- * @param TCircularList* ponteiro para a primeira lista
- * @param TCircularList* ponteiro para a segunda lista 
- * @return bool indicando se as listas são iguais ou não.
- * */
-bool TCircularList_equals(TCircularList*, TCircularList*);
-/**
- * Imprime uma lista circular.
- * @param TCircularList* ponteiro para a primeira lista
- * */
-void TCircularList_print(TCircularList*);
+
+typedef struct circular_list TCircularList;
+
+TCircularList *TCircularList_Create();
+void TCircularList_Print(TCircularList*);
+_Bool TCircularList_Insert_Begin(TCircularList*, int);
+_Bool TCircularList_Insert_End(TCircularList*, int);
+_Bool TCircularList_Insert(TCircularList*, int, char);
+_Bool TCircularList_remove_after_n_steps(TCircularList*, int);
+_Bool TCircularList_equals(TCircularList* , TCircularList* );
+void TCircularList_Scanear(TCircularList*);
+int TCircularList_CountNo(TCircularList*);
 
 #endif
